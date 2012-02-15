a868fc21800bd7b9ca2cb8f23062368091c89a6f
adam <financeCoding@gmail.com>
2012-02-15 13:54:54 -0800

    adding a dart util for webgl

---
+++ utils/webglUtils.dart
@@ -0,0 +1,72 @@
+#library('WebGLUtils');
+#import('dart:html');
+
+bool DEBUG=true;
+debugPrint(var p) {
+  if (DEBUG) {
+    print(p);
+  }  
+}
+
+debugPrintMethod(f) {
+  if (DEBUG) {
+    f();
+  }
+}
+
+WebGLProgram createProgram(WebGLRenderingContext gl, [List<WebGLShader> shaders]) {
+  // Create program
+  var program = gl.createProgram();
+  
+  // Iterate the shaders list
+  if (shaders is List<WebGLShader>) {
+    debugPrint("shaders.length = ${shaders.length}");
+    shaders.forEach((var shader) => gl.attachShader(program, shader));
+  }
+  
+  // Link the shader to program
+  gl.linkProgram(program);
+  
+  // Check the linked status
+  //var linked = gl.getProgramParameter(program, WebGLRenderingContext.LINK_STATUS);
+  //debugPrint("linked = ${linked}");
+  
+  return program;
+}
+
+WebGLShader loadShader(WebGLRenderingContext gl, String shaderSource, int shaderType) { 
+  // Create the shader object
+  var shader = gl.createShader(shaderType);
+  
+  // Load the shader source
+  gl.shaderSource(shader, shaderSource);
+  
+  // Compile the shader
+  gl.compileShader(shader);
+  
+  // Check the compile status
+  // NOTE: getShaderParameter maybe borken in minfrog or frog compiler. 
+  //var compiled = gl.getShaderParameter(shader, WebGLRenderingContext.COMPILE_STATUS);
+  //debugPrint("compiled = ${compiled}");
+  
+  return shader;
+}
+
+WebGLShader createShaderFromScriptElement(WebGLRenderingContext gl, String id) {
+  ScriptElement shaderScript = document.query(id);
+  String shaderSource = shaderScript.text;
+  int shaderType;
+  if (shaderScript.type == "x-shader/x-vertex") {
+    shaderType = WebGLRenderingContext.VERTEX_SHADER;
+  } else if (shaderScript.type == "x-shader/x-fragment") {
+    shaderType = WebGLRenderingContext.FRAGMENT_SHADER;
+  } else {
+    throw new Exception('*** Error: unknown shader type');
+  }
+  
+  return loadShader(gl, shaderSource, shaderType);
+}
+
+WebGLRenderingContext getWebGLContext(CanvasElement canvas) {
+  return canvas.getContext("experimental-webgl");
+}
--- webglFundamentals/webglFundamentals.dart
+++ webglFundamentals/webglFundamentals.dart
@@ -1,79 +1,9 @@
 #import('dart:html');
+#import('../utils/webglUtils.dart');
 
 class webglFundamentals {
 
-  bool DEBUG=true;
-  debugPrint(var p) {
-    if (DEBUG) {
-      print(p);
-    }  
-  }
-
-  debugPrintMethod(f) {
-    if (DEBUG) {
-      f();
-    }
-  }
-
-  
   webglFundamentals() {
-  }
-
-  WebGLProgram createProgram(WebGLRenderingContext gl, [List<WebGLShader> shaders]) {
-    // Create program
-    var program = gl.createProgram();
-    
-    // Iterate the shaders list
-    if (shaders is List<WebGLShader>) {
-      debugPrint("shaders.length = ${shaders.length}");
-      shaders.forEach((var shader) => gl.attachShader(program, shader));
-    }
-    
-    // Link the shader to program
-    gl.linkProgram(program);
-    
-    // Check the linked status
-    //var linked = gl.getProgramParameter(program, WebGLRenderingContext.LINK_STATUS);
-    //debugPrint("linked = ${linked}");
-    
-    return program;
-  }
-  
-  WebGLShader loadShader(WebGLRenderingContext gl, String shaderSource, int shaderType) { 
-    // Create the shader object
-    var shader = gl.createShader(shaderType);
-    
-    // Load the shader source
-    gl.shaderSource(shader, shaderSource);
-    
-    // Compile the shader
-    gl.compileShader(shader);
-    
-    // Check the compile status
-    // NOTE: getShaderParameter maybe borken in minfrog or frog compiler. 
-    //var compiled = gl.getShaderParameter(shader, WebGLRenderingContext.COMPILE_STATUS);
-    //debugPrint("compiled = ${compiled}");
-    
-    return shader;
-  }
-  
-  WebGLShader createShaderFromScriptElement(WebGLRenderingContext gl, String id) {
-    ScriptElement shaderScript = document.query(id);
-    String shaderSource = shaderScript.text;
-    int shaderType;
-    if (shaderScript.type == "x-shader/x-vertex") {
-      shaderType = WebGLRenderingContext.VERTEX_SHADER;
-    } else if (shaderScript.type == "x-shader/x-fragment") {
-      shaderType = WebGLRenderingContext.FRAGMENT_SHADER;
-    } else {
-      throw new Exception('*** Error: unknown shader type');
-    }
-    
-    return loadShader(gl, shaderSource, shaderType);
-  }
-  
-  WebGLRenderingContext getWebGLContext(CanvasElement canvas) {
-    return canvas.getContext("experimental-webgl");
   }
   
   void run() {
@@ -108,7 +38,7 @@ class webglFundamentals {
     // draw
     gl.drawArrays(WebGLRenderingContext.TRIANGLES, 0, 6);
     
-    //write("Ran webgl fundamental sample");
+    write("");
   }
 
   void write(String message) {
